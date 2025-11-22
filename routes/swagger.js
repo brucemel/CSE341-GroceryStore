a13e65dc2bf5e2c7823780360ca9526a6f417a8c
brucemel <mel24004@byui.edu>
2025-11-22 08:32:40 -0500

    testswager

--- routes/swagger.js
+++ routes/swagger.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger-output.json');
+const path = require('path');
+const fs = require('fs');
 
-router.use('/', swaggerUi.serve);
-router.get('/', swaggerUi.setup(swaggerDocument));
+const swaggerPath = path.join(__dirname, '..', 'swagger-output.json');
+
+// Verificar si el archivo existe
+if (fs.existsSync(swaggerPath)) {
+  const swaggerDocument = require(swaggerPath);
+  router.use('/api-docs', swaggerUi.serve);
+  router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+} else {
+  console.warn('swagger-output.json not found. Swagger docs will not be available.');
+  router.get('/api-docs', (req, res) => {
+    res.status(503).json({ 
+      error: 'Swagger documentation is not available',
+      message: 'Run "npm run swagger" to generate documentation'
+    });
+  });
+}
 
 module.exports = router;
